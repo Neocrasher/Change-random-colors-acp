c986ca672ddf6895d1044b3609c3c89a6e20c268
Neocrasher <blazeplayzjv@gmail.com>
2024-11-29 20:55:48 +0530

    acp done

---
+++ script.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    
+            var red = Math.floor(Math.random() * 256);
+            var green = Math.floor(Math.random() * 256);
+            var blue = Math.floor(Math.random() * 256);
+    document.body.style.backgroundColor = "rgb(" + red + "," + blue + "," + green + ")";
+    }
